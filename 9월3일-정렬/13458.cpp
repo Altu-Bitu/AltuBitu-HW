44840d8c20939953474d4106e68dc769af5d6d50
oohyj <89563205+oohyj@users.noreply.github.com>
2021-09-09 10:22:20 +0900

    [정렬]9월9일

---
+++ 9월3일-정렬/13458.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector <int> arr;
+
+int peo(int n, int a, int b) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i] - a;
+        int c = num % b;
+        int d = num / b;
+        if (c == 0) count += d + 1;
+        else count += d + 2;
+    }
+    return count;
+}
+int main()
+{
+    int n, a, b;
+    cin >> n;
+    arr.assign(n, 0);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> a >> b;
+
+    cout << peo(n, a, b) << endl;
+
+}
