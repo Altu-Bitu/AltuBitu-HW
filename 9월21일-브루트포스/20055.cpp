f9bafe0e34e09f02fea0dc8cc6ed3fa848497d05
oohyj <89563205+oohyj@users.noreply.github.com>
2021-09-27 16:17:50 +0900

    [브루트포스]9월27일

---
+++ 9월21일-브루트포스/20055.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector <int> dur;
+	for (int i = 0; i < 2*n; i++) {
+		cin >> dur[i];
+	}
+}
